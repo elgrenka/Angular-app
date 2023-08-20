8bdf57b16cb97608663ad45725830ac995c3240d
elgrenka <bugigen@yandex.ru>
2023-08-20 21:09:33 +0400

    before lazy

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,11 +13,17 @@ export class HeaderComponent implements OnInit {
     constructor(private authService: AuthService, private router: Router) { }
 
     ngOnInit(): void {
+        // this.updateLoginStatus();
+        this.isLoggedIn = this.authService.isLoggedIn();
+    }
+
+    private updateLoginStatus(): void {
         this.isLoggedIn = this.authService.isLoggedIn();
     }
 
     logout(): void {
         this.authService.logout();
+        // this.updateLoginStatus();
         this.isLoggedIn = false;
 
         this.router.navigate(['/login']);
